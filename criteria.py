deb545db5edeeb5927545dbff0375a647e8d178c
hina3271 <ys.kim@postech.ac.kr>
2025-06-21 16:02:46 +0900

    update

--- criteria.py
+++ criteria.py
@@ -53,8 +53,11 @@ class SpectralLoss(torch.nn.Module):
         x_mag, x_pha, x_com = mag_pha_stft(x, self.fft_size, self.hop_size, self.win_length, compress_factor=self.compress_factor, center=True)
         y_mag, y_pha, y_com = mag_pha_stft(y, self.fft_size, self.hop_size, self.win_length, compress_factor=self.compress_factor, center=True)
 
-        loss_mag = F.mse_loss(x_mag, y_mag)
-        loss_com = F.mse_loss(x_com, y_com)
+        loss_mag = F.l1_loss(torch.log(x_mag + 1e-8), torch.log(y_mag + 1e-8))
+        loss_com = torch.norm(x_mag - y_mag, p="fro") / torch.norm(y_mag, p="fro")
+
+        # loss_mag = F.mse_loss(x_mag, y_mag)
+        # loss_com = F.mse_loss(x_com, y_com)
         loss_ip, loss_gd, loss_iaf = phase_losses(x_pha, y_pha)
         loss_pha = loss_ip + loss_gd + loss_iaf
 
